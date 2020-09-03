37a02755d62bfb87aa45bec5c987c3f994a5fe81
Sarah <sarah.b.avram@gmail.com>
2020-09-03 16:13:50 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(20));
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(80));
+        }
+    }
+}
---
+++ TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrTemp)
+        {
+            double celsTemp = (fahrTemp - 32) / 1.8;
+            return Math.Round(celsTemp, 1);
+        }
+
+        public static double CelsiusToFahrenheit(double celsTemp)
+        {
+            double fahrTemp = (celsTemp * 1.8) + 32;
+            return Math.Round(fahrTemp, 1);
+        }
+    }
+}
